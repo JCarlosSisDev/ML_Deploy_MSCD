a68284e91d503fe2e699ccd68aac280daeaf0f57
JCarlosSisDev <jcarlos.quisbert.sis@gmail.com>
2023-02-24 10:08:39 -0400

    Modificaciones y pruebas, actualización del tid-regression-model

--- section-06-model-serving-api/house-prices-api/app/api.py
+++ section-06-model-serving-api/house-prices-api/app/api.py
@@ -6,14 +6,12 @@ import pandas as pd
 from fastapi import APIRouter, HTTPException
 from fastapi.encoders import jsonable_encoder
 from loguru import logger
+from app import __version__, schemas
+from app.config import settings
 from regression_model import __version__ as model_version
 from regression_model.predict import make_prediction
 
-from app import __version__, schemas
-from app.config import settings
-
 api_router = APIRouter()
-
 
 @api_router.get("/health", response_model=schemas.Health, status_code=200)
 def health() -> dict:
